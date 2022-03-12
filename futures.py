7600b35a340e272c37961b5a8c38825a1540ca19
bellabula <zz225864@gmail.com>
2022-03-13 01:12:13 +0800

    save in dict

--- futures.py
+++ futures.py
@@ -1,57 +1,57 @@
-from asyncio.windows_events import NULL
-from email import header
 import requests
 from bs4 import BeautifulSoup
 from datetime import datetime, timedelta
-import pandas as pd
 
 def crawl(date):
     r = requests.get('https://www.taifex.com.tw/cht/3/futContractsDate?queryDate={}%2F{}%2F{}'.format(date.year, date.month, date.day))
     if r.status_code == requests.codes.ok:
         soup = BeautifulSoup(r.text, 'html.parser')
-        if soup.find_all('td', class_='13red'):
-            out = NULL
-        else:
+        try:
             table = soup.find('table', class_='table_f')
             rows = table.find_all('tr')
-            first_colname = ['']*3
-            for _ in list(rows.pop(0).stripped_strings):
-                first_colname += [_] * 6
-            second_colname = ['']*3
-            for _ in list(rows.pop(0).stripped_strings):
-                second_colname += [_] * 2
-            third_colname = [th.text for th in rows.pop(0).find_all('th')]
-            data = []
-            row_count = 1
-            for row in rows:
-                ths = row.find_all('th')
-                if row_count%3 == 1 :
-                    d_head = [th.text.strip() for th in row.find_all('th')]
-                    if len(d_head) == 3:
-                        pass
-                    else:
-                        d_head = [d_head[0]]*(3-len(d_head)) + d_head
-                else:
-                    d_head = d_head[:2] + [row.find_all('th')[-1].text.strip()]
+        except AttributeError:
+            print('no data for', date.date())
+            return
+        
+        data = {}
+        for row in rows[3:]:
+            d = list(row.stripped_strings)
 
-                d = d_head + [td.text.strip() for td in row.find_all('td')]
-                row_count += 1
-                data.append(d)
-                # raise SystemExit
-            out = [first_colname, second_colname, third_colname] + data
+            if d[0] == '期貨':
+                break
+
+            if len(d) == 15:
+                product = d[1]
+                row_data = d[1:]
+            else:
+                row_data = [product] + d
+
+            row_data = row_data[:2] + [int(d.replace(',', '')) for d in row_data[2:]]
+            col_name = ['商品', '身份別', '交易多方口數', '交易多方契約金額', '交易空方口數', '交易空方契約金額', '交易多空淨額口數','交易多空淨額契約金額',
+                        '未平倉多方口數', '未平倉多方契約金額', '未平倉空方口數', '未平倉空方契約金額', '未平倉多空淨額口數','未平倉多空淨額契約金額']
+
+            # product - who - what
+            product = row_data[0]
+            who = row_data[1]
+            contents = {col_name[i]:row_data[i] for i in range(2, len(row_data))}
+
+            if product not in data:
+                data[product] = {who:contents}
+            else:
+                data[product][who] = contents
+        return data
     else:
         print('connection error')
-    return out
 
 date = datetime.today()
 get_days = 5 # 期貨交易所只有近3年資料 ()
+data = {}
 while True:
-    data = crawl(date)
-    if data == NULL:
-        print(date.date(), '例假日查無資料')
+    daily_data = crawl(date)
+    if daily_data == None:
+        pass
     else:
-        df = pd.DataFrame(data)
-        df.to_csv(f'futures_{date.date()}.csv', index=False, header=False)
+        data[date.strftime('%Y/%m/%d')] = daily_data
     date = date - timedelta(days=1)
     if date < datetime.today() - timedelta(days=get_days):
         break
